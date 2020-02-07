0c1795f14d73bf7c4bc926ed8dc6a3ec2053ccd3
sonnynajar <sonny.najar@gmail.com>
2020-02-07 13:11:27 -0800

    starting gameshow questions for 3Ds

---
+++ ts-camp-materials/scribblings/gameshow-3d-exploration.rkt
@@ -0,0 +1,56 @@
+#lang scribble/manual
+
+@title{Game Show}
+
+Use these questions during Game Show time. You can ask any/all the questions in each day's section.
+
+@section{Monday}
+
+@itemlist[@item{@bold{Name 4 different environment presets.}
+                    @itemlist[@item{@italic{Any 4 of these:  contact, egypt, checkerboard, forest, goaland, yavapai, goldmine, threetowers, poison, arches, tron, japan, dream, volcano, starry, or osiris.}}]}
+          @item{@bold{Name 3 different types of dressings (not the ones you put on salads).}
+                    @itemlist[@item{@italic{Any 3 of these: cubes, pyramids, cylinders, towers, mushrooms, trees, apparatus, or torii.}}]}
+          @item{@bold{What is a keyword?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}]
+
+
+@section{Tuesday}
+
+@itemlist[@item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}]
+@section{Wednesday}
+
+@itemlist[@item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}]
+@section{Thursday}
+
+@itemlist[@item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}
+          @item{@bold{?}
+                    @itemlist[@item{@italic{?}}]}]
